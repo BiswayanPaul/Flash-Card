9514586950e22362d5e80c807509de9a0fab415a
Biswayan Paul <biswayanpaul2015@gmail.com>
2024-08-14 01:16:23 +0530

    quick fix 3

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,17 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-export type ExtendedUser = DefaultSession["user"] & {
-    role: UserRole;
-  };
-  
-  declare module "next-auth" {
-    interface Session {
-      user: ExtendedUser;
-      isTwoFactorEnabled: boolean;
-    }
-  }
-  
-  // NOTE: This file should not be edited
-  // see https://nextjs.org/docs/basic-features/typescript for more information.
-  
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
